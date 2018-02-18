6aa97d54178b3a7a82292c99be25cff2c9029a30
Yeison Daza <jahir045@gmail.com>
2018-02-17 22:09:26 -0500

    #format: prettier

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,52 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-const Menu = (props) => (
-    <nav id="menu">
-        <div className="inner">
-            <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
-            </ul>
-        </div>
-        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
-    </nav>
-)
+const Menu = props => (
+  <nav id="menu">
+    <div className="inner">
+      <ul className="links">
+        <li>
+          <Link onClick={props.onToggleMenu} to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/landing">
+            Landing
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/generic">
+            Generic
+          </Link>
+        </li>
+        <li>
+          <Link onClick={props.onToggleMenu} to="/elements">
+            Elements
+          </Link>
+        </li>
+      </ul>
+      <ul className="actions vertical">
+        <li>
+          <a href="#" className="button special fit">
+            Get Started
+          </a>
+        </li>
+        <li>
+          <a href="#" className="button fit">
+            Log In
+          </a>
+        </li>
+      </ul>
+    </div>
+    <a className="close" onClick={props.onToggleMenu} href="javascript:;">
+      Close
+    </a>
+  </nav>
+);
 
 Menu.propTypes = {
-    onToggleMenu: React.PropTypes.func
-}
+  onToggleMenu: React.PropTypes.func,
+};
 
-export default Menu
+export default Menu;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,59 +1,62 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import '../assets/scss/main.scss'
-import Header from '../components/Header'
-import Menu from '../components/Menu'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
+import React from 'react';
+import Link from 'gatsby-link';
+import '../assets/scss/main.scss';
+import Header from '../components/Header';
+import Menu from '../components/Menu';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 class Template extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuVisible: false,
+      loading: 'is-loading',
+    };
+    this.handleToggleMenu = this.handleToggleMenu.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuVisible: false,
-            loading: 'is-loading'
-        }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => {
+      this.setState({ loading: '' });
+    }, 100);
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
+  }
 
-    componentDidMount () {
-        this.timeoutId = setTimeout(() => {
-            this.setState({loading: ''});
-        }, 100);
-    }
+  handleToggleMenu() {
+    this.setState({
+      isMenuVisible: !this.state.isMenuVisible,
+    });
+  }
 
-    componentWillUnmount () {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    }
+  render() {
+    const { children } = this.props;
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
-
-    render() {
-        const { children } = this.props
-
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
-                    {children()}
-                    <Contact />
-                    <Footer />
-                </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
-            </div>
-        )
-    }
+    return (
+      <div
+        className={`body ${this.state.loading} ${
+          this.state.isMenuVisible ? 'is-menu-visible' : ''
+        }`}
+      >
+        <div id="wrapper">
+          <Header onToggleMenu={this.handleToggleMenu} />
+          {children()}
+          <Contact />
+          <Footer />
+        </div>
+        <Menu onToggleMenu={this.handleToggleMenu} />
+      </div>
+    );
+  }
 }
 
 Template.propTypes = {
-    children: React.PropTypes.func
-}
+  children: React.PropTypes.func,
+};
 
-export default Template
+export default Template;
