cbf27b02fba26ea1e558782e9fddc294db29846a
Yeison Daza <jahir045@gmail.com>
2018-02-18 10:37:46 -0500

    '#form'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,25 +4,27 @@ const Contact = props => (
   <section id="contact">
     <div className="inner">
       <section>
-        <form method="post" action="#">
+        <form name="contact" netlify netlify-honeypot="bot-field">
+          <div style={{ display: 'none' }}>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </div>
           <div className="field half first">
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">Nombre</label>
             <input type="text" name="name" id="name" />
           </div>
           <div className="field half">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Correo</label>
             <input type="text" name="email" id="email" />
           </div>
           <div className="field">
-            <label htmlFor="message">Message</label>
+            <label htmlFor="message">Mensaje</label>
             <textarea name="message" id="message" rows="6" />
           </div>
           <ul className="actions">
             <li>
-              <input type="submit" value="Send Message" className="special" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
+              <input type="submit" value="Enviar" className="special" />
             </li>
           </ul>
         </form>
@@ -32,20 +34,22 @@ const Contact = props => (
           <div className="contact-method">
             <span className="icon alt fa-envelope" />
             <h3>Email</h3>
-            <a href="#">information@untitled.tld</a>
+            <a href="mailto:hablame@lauramaestre.com">
+              hablame@lauramaestre.com
+            </a>
           </div>
         </section>
         <section>
           <div className="contact-method">
             <span className="icon alt fa-phone" />
-            <h3>Phone</h3>
-            <span>(000) 000-0000 x12387</span>
+            <h3>Número</h3>
+            <span>(+57) 000-0000</span>
           </div>
         </section>
         <section>
           <div className="contact-method">
             <span className="icon alt fa-home" />
-            <h3>Address</h3>
+            <h3>Dirección</h3>
             <span>
               1234 Somewhere Road #5432<br />
               Nashville, TN 00000<br />
