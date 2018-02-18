7cd35ec8172aaabe77a5e6b6892e7f78451fed99
Yeison Daza <jahir045@gmail.com>
2018-02-18 18:22:42 -0500

    '#timer'

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Countdown from 'react-countdown-now';
 
 const BannerLanding = ({ title, description }) => (
   <section id="banner">
@@ -9,9 +10,34 @@ const BannerLanding = ({ title, description }) => (
       <div className="content">
         <p>{description}</p>
       </div>
+      <div id="content">
+        <Countdown
+          date={new Date('February 24, 2018 09:00:00')}
+          renderer={({ days, hours, minutes, seconds, completed }) => {
+            return (
+              !completed && (
+                <div className="countdown">
+                  <div className="time">
+                    {days}
+                    <span>Días</span>
+                  </div>
+                  <div className="time">
+                    {hours}
+                    <span>Horas</span>
+                  </div>
+                  <div className="time">
+                    {minutes}
+                    <span>Min</span>
+                  </div>
+                </div>
+              )
+            );
+          }}
+        />,
+      </div>
       <ul className="actions">
         <li>
-          <a href="#one" className="button special">
+          <a href="#contact" className="button special">
             Registrarme
           </a>
         </li>
