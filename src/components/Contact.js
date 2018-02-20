84c75fb870a5093e4aeb03130968332c3e2c1048
Yeison Daza <jahir045@gmail.com>
2018-02-20 07:32:43 -0500

    '#fix: path thanks'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = props => (
           data-netlify="true"
           netlify-honeypot="bot-field"
           method="post"
-          action="/gracias/"
+          action="/gracias"
         >
           <div style={{ display: 'none' }}>
             <label>
