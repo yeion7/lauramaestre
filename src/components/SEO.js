19682eb530ac84b19cbdd433f1f7925420f9d4a3
Yeison Daza <jahir045@gmail.com>
2018-02-21 07:07:34 -0500

    '#fix: seo'

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,6 +6,7 @@ const FavIcon = ({ title, description, url, isPost, image }) => {
   return (
     <Helmet>
       {/* General tags */}
+      <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image || lauraImage} />
 
