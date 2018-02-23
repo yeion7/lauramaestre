204d92392fd000b59f118ca9a44157e885f71249
Yeison Daza <jahir045@gmail.com>
2018-02-22 19:46:33 -0500

    '#add: admin'

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
