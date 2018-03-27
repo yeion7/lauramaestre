4cf062800466bb3f31b1cfb312e22763a175131e
Yeison Daza <jahir045@gmail.com>
2018-03-26 20:24:53 -0500

    '#add: questions'

---
+++ src/components/Questions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Questions = ({ color, questions = [] }) => {
+  return (
+    <section id="questions" className={color}>
+      <div className="inner">
+        <header>
+          <h2>¿Has tenido alguna de estas situaciones en tu vida?</h2>
+        </header>
+        <ul className="alt row">
+          {questions.map(question => (
+            <li>
+              <h3>
+                <i className="fa fa-2x fa-check" style={{ color: 'white' }} />
+                {question}
+              </h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Questions;
--- src/pages/eventos/sanando-raices-villavicencio.js
+++ src/pages/eventos/sanando-raices-villavicencio.js
@@ -6,14 +6,16 @@ import Details from '../../components/Details';
 import Faq from '../../components/Faq';
 import Feautures from '../../components/Feautures';
 import Testimonial from '../../components/Testimonial';
+import Questions from '../../components/Questions';
 import Image from '../../assets/images/landing.jpg';
 
 class Landing extends React.Component {
   render() {
     const { location } = this.props;
+    const color = 'blue';
     return (
       <div>
-        <FavIcon type="blue" />
+        <FavIcon type={color} />
         <SEO
           title="Laura Maestre | Taller Sanando Tus Raíces"
           description="¿Te has preguntado porqué no tienes los resultados que quieres en tu vida? ¿Has sentido que hay historias de tu familia que se repiten?"
@@ -24,7 +26,7 @@ class Landing extends React.Component {
           description="¿Te has preguntado porqué no tienes los resultados que quieres en tu vida? ¿Has sentido que hay historias de tu familia que se repiten?"
           time="May 5, 2018 09:00:00"
           image={Image}
-          color="blue"
+          color={color}
         />
 
         <div id="main">
@@ -32,26 +34,36 @@ class Landing extends React.Component {
             achievements={[
               {
                 icon: 'fa-star',
-                text: `Realizarás tu árbol genealógico para descubrir las historias y proyectos que has cargado de tus antepasados.`,
+                text: `Sanarás las memorias de su familia al  perdonar situaciones que han sido difíciles, como abandonos, abusos, rechazos, etc.`,
               },
               {
                 icon: 'fa-medkit',
-                text: `Cortarás las cargas que te entregaron tus antepasados mediante un proceso de liberación.`,
+                text: `Cortarás las cargas que te entregaron tus antepasados  mediante un proceso de liberación y transmutación.`,
               },
               {
                 icon: 'fa-plane',
-                text: `Crearás nuevos permisos y proyectos para crear la vida que deseas.`,
+                text: `Crearás nuevos permisos y proyectos para comenzar a construir la vida que deseas.`,
               },
             ]}
-            color="blue"
+            color={color}
+          />
+          <Questions
+            questions={[
+              `¿Por más que deseas tener una pareja siempre te encuentras el mismo patrón de pareja que no te hace feliz?`,
+              `¿Has sentido que hay historias de tu familia que se repiten?`,
+              `¿Sientes que a lo que te dedicas en vida es realmente a algo que no amas o no te generas ingresos económicos?`,
+              `¿Hay enfermedades que se repiten en tu familia?`,
+              `Si alguna de las preguntas contestaste que sí…entonces en el Taller Sanando tus Raíces y a través de tu árbol genealógico y tu concepción encontrarás las respuestas.`,
+            ]}
+            color={color}
           />
           <Details
             place="Villavicencio, Meta."
             date="5 de Mayo"
             price="$130.000"
-            color="blue"
+            color={color}
           />
-          <Faq color="blue" />
+          <Faq color={color} />
         </div>
       </div>
     );
